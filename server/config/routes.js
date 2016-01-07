dbd58e55e73995e906e379107f6817b306d5f3fa
Ian <ian.bulmer89@gmail.com>
2016-01-07 11:06:23 -0800

    server.js no longer blank

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,8 +3,8 @@ var userController = require('../users/userController.js');
 var helpers = require('./helpers.js');
 
 module.exports = function (app, express) {
-  app.post('/api/users/signin', userController.signin);
-  app.post('/api/users/signup', userController.signup);
+  //app.post('/api/users/signin', userController.signin);
+  //app.post('/api/users/signup', userController.signup);
   //app.get('/api/users/signedin', userController.checkAuth);
 
   // authentication middleware used to decode token and made available on the request
--- server/users/userController.js
+++ server/users/userController.js
@@ -1,1 +1,9 @@
-userController.js
+var User = require('./userModel.js');
+var Q = require('q');
+
+var findUser = Q.nbind(User.findOne, User);
+var createUser = Q.nbind(User.create, User);
+
+module.exports = {
+
+};
