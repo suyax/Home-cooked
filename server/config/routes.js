7a95cf5965661e984292952822371d3d258a8419
suya xu <tataxusuya@hotmail.com>
2016-01-07 08:37:56 -0800

    handle all other route treat as a get request for all meals

--- server/config/routes.js
+++ server/config/routes.js
@@ -10,6 +10,8 @@ module.exports = function (app, express) {
   // authentication middleware used to decode token and made available on the request
   app.get('/api/meals/', mealController.allMeals);
   app.post('/api/meals/', mealController.newMeal);
+  //handle all other route treat as a get request for all meals
+  app.get('api/*', mealController.allMeals);
 
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
