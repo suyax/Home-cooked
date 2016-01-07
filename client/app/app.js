ee741fa25ca716d4419407fd450d05bc762a0329
Richard Castro <rscastro13@gmail.com>
2016-01-06 19:43:53 -0800

    index file and app.js for uirouter

---
+++ client/app/app.js
@@ -0,0 +1,76 @@
+angular.module('homecooked', [
+  'homecooked.services',
+  'homecooked.meals',
+  'homecooked.create',
+  'homecooked.auth',
+  'ngRoute', 
+  'ui.router'
+])
+.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/meals');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/auth/signin.html',
+            controller: 'AuthController',
+        })
+
+        .state('meals', {
+            url: '/meals',
+            templateUrl: 'app/meals/meals.html',
+            controller: 'MealsController',
+            authenticate: true
+        })
+        
+        .state('signin', {
+            url: '/signin',
+            templateUrl: 'app/auth/signin.html',
+            controller: 'AuthController'
+        })
+
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'app/auth/signup.html',
+            controller: 'AuthController'
+        })
+
+        .state('submit', {
+            url: '/submit',
+            templateUrl: 'app/submit/submit.html',
+            controller: 'SubmitController',
+            authenticate: true
+        })
+
+        .state('logout', {
+            url: '/logout',
+            templateUrl: 'app/auth/signin.html',
+            controller: 'AuthController'
+        })
+})
+
+.factory('AttachTokens', function ($window) {
+  var attach = {
+    request: function (object) {
+      var jwt = $window.localStorage.getItem('com.shortly');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+.run(function ($rootScope, $state, $location, Auth) {
+  $rootScope.$on('$stateChangeStart', function (evt, toState, toParams, fromState, fromParams) {
+    if (toState.authenticate && !Auth.isAuth()) {
+      $state.transitionTo('signin');
+      evt.preventDefault();
+      //$location.path('/signin');
+    }
+  });
+});
