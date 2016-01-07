9028db13b739ae0e2778ab47bf48ae23a66490bb
Ian <ian.bulmer89@gmail.com>
2016-01-06 17:00:35 -0800

    added the middleware

--- server/server.js
+++ server/server.js
@@ -11,7 +11,14 @@ require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
-app.listen(8000);
+app.listen(8000, function(err, success){
+	if(err){
+		console.log(err);
+	} else {
+		console.log('listening on port 8080');
+	}
+
+});
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
---
+++ server/users/userModel.js
@@ -0,0 +1,14 @@
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('users', UserSchema);
