519ea054d4478754aef7d0e5786b989ce83f0e84
Hack Reactor Students <accounts@hackreactor.com>
2016-01-07 19:35:49 -0800

    all routes are working

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,15 +3,13 @@ var userController = require('../users/userController.js');
 var helpers = require('./helpers.js');
 
 module.exports = function (app, express) {
+  //handle get and post request from client and pass them to controller
   app.post('/api/users/signin', userController.signin);
   app.post('/api/users/signup', userController.signup);
-  //app.get('/api/users/signedin', userController.checkAuth);
 
-  // authentication middleware used to decode token and madelable on the request
   app.get('/api/meals', mealController.allMeals);
   app.post('/api/meals', mealController.newMeal);
-  //handle all other route treat as a get request for all meals
-  app.get('api/*', mealController.allMeals);
+  app.get('/api/*', mealController.allMeals);
 
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
--- server/meals/mealController.js
+++ server/meals/mealController.js
@@ -1,12 +1,12 @@
 var Meal = require('./mealModel.js');
-    Q = require('q');
+Q = require('q');
 
-    var findMeal = Q.denodeify(Meal.findOne, Meal);
-    var createMeal = Q.denodeify(Meal.create, Meal);
-    var findAllMeals = Q.denodeify(Meal.find, Meal);
+var findMeal = Q.nbind(Meal.findOne, Meal);
+var createMeal = Q.nbind(Meal.create, Meal);
+var findAllMeals = Q.nbind(Meal.find, Meal);
 
   module.exports = {
-    //return all meals as respond to client
+    //returns all meals in our database as the response
     allMeals: function (req, res, next) {
       findAllMeals({})
         .then(function (meals) {
@@ -16,7 +16,7 @@ var Meal = require('./mealModel.js');
           next(error);
         });
     },
-
+    //returns the meal as the response to the client
     newMeal: function (req, res, next) {
       var meal = {
         title : req.body.title,
@@ -24,61 +24,19 @@ var Meal = require('./mealModel.js');
         description : req.body.description,
         date : req.body.date,
         time : req.body.time
-       };
-          
-        // (function(){
-        //   console.log("got to the anon function");
-        //   return createMeal(meal);
-        // })()
-        // .then(function(createdMeal){
-        //   res.json(createdMeal);
-        // });
-        // console.log("created meal is: ",createdMeal);
-        // )
-        // .then(function (createdMeal) {
-        //   if (createdMeal) {
-        //     res.json(createdMeal);
-        //   }
-        // })
-        // .fail(function (error) {
-        //   next(error);
-        // });
-    /*    findMeal({title:"fake title"})
-          .then(function (meal) {
-            return createMeal({
-              title : req.body.title,
-              picture : req.body.picture,
-              description : req.body.description,
-              date : req.body.date,
-              time : req.body.time
-              });
-          })
-          .then(function (user) {
-                  // create token to send back for aut
-                  res.json(user);
-                })
-                .fail(function (error) {
-                  next(error);
-               });
-              }*/
-
-
-        function(){
-          return createMeal({
-                  title: meal.title,
-                  picture: meal.picture,
-                  description: meal.description,
-                  date: meal.date,
-                  time: meal.time
-                })
-        }()
-        .then(function(createdMeal){
-          if (createdMeal){
-            res.json(createdMeal);
-          }
-        })
-        .fail(function(error){
-          next(error);
-        });v 
+      };
+      var makeMeal = function (meal) {
+        return createMeal(meal);
+      };
+      makeMeal(meal)
+      .then(function(createdMeal){
+        if (createdMeal){
+          res.json(createdMeal);
+        }
+      })
+      .fail(function(error){
+        next(error);
+      });
+  }
 };
 
