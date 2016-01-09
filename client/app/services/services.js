6cc088980c832eef826cf049eb4259beb81ec6b8
Richard Castro <rscastro13@gmail.com>
2016-01-09 11:25:07 -0800

    signin and signup done

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,1 +1,46 @@
 angular.module('homecooked.services', [])
+
+.factory('Auth', function ($http, $location, $window) {
+  var signin = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signin',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var signup = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+
+
+  var isAuth = function () {
+    return !!$window.localStorage.getItem('com.shortly');
+  };
+
+  var signout = function () {
+    $window.localStorage.removeItem('com.shortly');
+    $window.localStorage.removeItem('global.user');
+
+    $location.path('/signin');
+  };
+
+
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+});
