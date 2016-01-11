1273c7c788f0ad1d113fc10351074d5a0e28a99f
Ian <ian.bulmer89@gmail.com>
2016-01-10 20:06:47 -0800

    added comments and documentation

--- server/meals/mealController.js
+++ server/meals/mealController.js
@@ -12,7 +12,7 @@ var findAllMeals = Q.nbind(Meal.find, Meal);
 
 //export module allMeals and newMeal
   module.exports = {
-    //returns all meals in our database as the response
+    //returns all meals the database as the response.
     allMeals: function (req, res, next) {
       findAllMeals({})
         .then(function (meals) {
@@ -23,8 +23,9 @@ var findAllMeals = Q.nbind(Meal.find, Meal);
         });
     },
 
-    //returns the meal as the response to the client
+    //creates a meal that gets saved to the database.
     newMeal: function (req, res, next) {
+      //variable meal takes the request body and transforms it into an object
       var meal = {
         title : req.body.title,
         picture : req.body.picture,
@@ -33,11 +34,14 @@ var findAllMeals = Q.nbind(Meal.find, Meal);
         time : req.body.time,
         user : req.body.user
       };
+      //create meal is wrapped in a function to handle asynchronous nature of createMeal
       var makeMeal = function (meal) {
         return createMeal(meal);
       };
+      //makeMeal is invoked which saves a meal to the database
       makeMeal(meal)
       .then(function(createdMeal){
+        //upon successful save to the database the server responds with data saved to the database in JSON form
         if (createdMeal){
           res.json(createdMeal);
         }
